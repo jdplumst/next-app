4d3e211b673f6cd86a5e36a3050ecfc4bbd210bb
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-02-25 13:46:57 -0500

    Add page for individual Posts

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,7 @@
+export default function IndividualPost() {
+  return (
+    <div>
+      <h1>This is a post!</h1>
+    </div>
+  );
+}
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,4 +1,5 @@
 import { InferGetStaticPropsType } from "next";
+import Link from "next/link";
 
 interface Post {
   id: string;
@@ -20,11 +21,11 @@ export default function Posts({
   return (
     <div>
       {posts.map((post) => (
-        <div
-          key={post.id}
-          className="bg-red-500 my-5 text-center w-1/2 mx-auto">
-          <strong>{post.title}</strong> by {post.first_name} {post.last_name}
-        </div>
+        <Link key={post.id} href={`posts/${post.id}`}>
+          <div className="bg-red-500 my-5 text-center w-1/2 mx-auto">
+            <strong>{post.title}</strong> by {post.first_name} {post.last_name}
+          </div>
+        </Link>
       ))}
     </div>
   );
