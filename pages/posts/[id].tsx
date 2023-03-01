e7b7f36d003b11161dab887cb5c698bc3141f311
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-03-01 01:15:49 -0500

    Allow user to update a Post

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,4 +1,5 @@
 import { InferGetStaticPropsType } from "next";
+import { useState } from "react";
 
 interface Post {
   id: string;
@@ -31,6 +32,47 @@ export async function getStaticProps(context: any) {
 export default function IndividualPost({
   post
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const [title, setTitle] = useState(post.title);
+  const [content, setContent] = useState(post.content);
+  const [firstName, setFirstName] = useState(post.first_name);
+  const [lastName, setLastName] = useState(post.last_name);
+
+  const handleTitle = (e: any) => {
+    setTitle(e.target.value);
+  };
+
+  const handleContent = (e: any) => {
+    setContent(e.target.value);
+  };
+
+  const handleFirstName = (e: any) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleLastName = (e: any) => {
+    setLastName(e.target.value);
+  };
+
+  const updatePost = async () => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/post/${post.id}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          title: title,
+          content: content,
+          first_name: firstName,
+          last_name: lastName
+        })
+      }
+    );
+    const data = await response.json();
+    return data;
+  };
+
   return (
     <div className="p-4">
       <h1 className="text-3xl font-bold mb-4">This is a post!</h1>
@@ -42,31 +84,40 @@ export default function IndividualPost({
         <div className="flex gap-4">
           <label className="text-right w-20 inline-block">Title: </label>
           <input
-            defaultValue={post.title}
+            onInput={handleTitle}
+            value={title}
             className="w-1/2 border-2 border-black"
           />
         </div>
         <div className="flex gap-4">
           <label className="text-right w-20 inline-block">Content: </label>
           <input
-            defaultValue={post.content}
+            onInput={handleContent}
+            value={content}
             className="w-1/2 border-2 border-black"
           />
         </div>
         <div className="flex gap-4">
           <label className="text-right w-20 inline-block">First Name: </label>
           <input
-            defaultValue={post.first_name}
+            onInput={handleFirstName}
+            value={firstName}
             className="w-1/2 border-2 border-black"
           />
         </div>
         <div className="flex gap-4">
           <label className="text-right w-20 inline-block">Last Name: </label>
           <input
-            defaultValue={post.last_name}
+            onInput={handleLastName}
+            value={lastName}
             className="w-1/2 border-2 border-black"
           />
         </div>
+      </div>
+      <div className="flex justify-center mt-10">
+        <button onClick={updatePost} className="bg-purple-500 w-fit p-4">
+          Update
+        </button>
       </div>
     </div>
   );
